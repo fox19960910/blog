61270fc92b71fbba11f58b5cae17261f1e35cf3d
Sotatek-Tungle2 <tung.le2@sotatek.com>
2022-12-26 17:49:24 +0700

    ep 10

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { engine } = require("express-handlebars");
+const morgan = require("morgan");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+//HTTP logger
+app.use(morgan("combined"));
+
+// Template engine
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "/resouces/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
