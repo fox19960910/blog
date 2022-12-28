78f2af9904e9c36efdb7d8a57f96b68d38f0b91d
Sotatek-Tungle2 <tung.le2@sotatek.com>
2022-12-28 15:55:40 +0700

    19,20: MVC

---
+++ src/app/controllers/NewController.js
@@ -0,0 +1,13 @@
+class NewController {
+  // [GET] /news
+  index(req, res) {
+    res.render("news");
+  }
+
+  // [GET] /news/:slug
+  show(req, res) {
+    res.send("Detail");
+  }
+}
+
+module.exports = new NewController();
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { engine } = require("express-handlebars");
 const morgan = require("morgan");
 const path = require("path");
+const route = require("./routes");
 
 const app = express();
 const port = 3000;
@@ -25,19 +26,7 @@ app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "/resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/search", (req, res) => {
-  res.render("search");
-});
-
-app.post("/search", (req, res) => {
-  const data = req.body;
-  console.log(data);
-  res.send(data);
-});
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
