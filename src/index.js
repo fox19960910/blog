ea464bf425e4469fafced5dbf68a1f843dd9cddd
Sotatek-Tungle2 <tung.le2@sotatek.com>
2022-12-28 14:21:17 +0700

    14 -> 18: GET,POST method

--- src/index.js
+++ src/index.js
@@ -5,16 +5,38 @@ const path = require("path");
 
 const app = express();
 const port = 3000;
+
+// Use img
+app.use(express.static(path.join(__dirname, "/public")));
+
+// use middkeware
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
 //HTTP logger
 app.use(morgan("combined"));
 
 // Template engine
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "/resouces/views"));
+app.set("views", path.join(__dirname, "/resources/views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+
+app.post("/search", (req, res) => {
+  const data = req.body;
+  console.log(data);
+  res.send(data);
 });
 
 app.listen(port, () => {
